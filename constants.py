c83e5641f669ada6cb6ea67d84823557444845ab
Shail Shah <shailshah9@gmail.com>
2023-10-04 21:33:23 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ sent_from=''
 gmail_user=''
 gmail_password=''
 subject='Looking for a Full Time Position at %s'
-body="Hello %s,\n\nHope you are having a nice day.\n\nI am <>, pursuing my MS in computer science at Santa Clara University. I will be graduating in June 2019 and currently looking for full – time opportunities in the Software Engineering, Software Development, and Full-stack application development at %s. \n\nI have experience in Java, Android, JavaScript, and SQL. If you have any exciting opportunities, then please look at my resume attached below. I am available to start on 17th June 2019. \n\nLooking forward to hearing from you.\n\n Thanks and Regards,\nPriyanshi"
+body="Hello %s,\n\nHope you are having a nice day.\n\nI am <>, pursuing my MS in computer science at Santa Clara University. I will be graduating in June 2019 and currently looking for full – time opportunities in the Software Engineering, Software Development, and Full-stack application development at %s. \n\nI have experience in Java, Android, JavaScript, and SQL. If you have any exciting opportunities, then please look at my resume attached below. I am available to start on 17th June 2019. \n\nLooking forward to hearing from you.\n\n Thanks and Regards,\n<>"
 resume=''
